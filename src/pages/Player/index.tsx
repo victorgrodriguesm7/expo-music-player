a1d6c91d1410dde408ad73f0ea48a01dc32e9f96
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-01 10:42:52 -0300

    Player page created

---
+++ src/pages/Player/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+import * as MediaLibrary from 'expo-media-library';
+
+export default function Player() {
+    
+    return (
+        <View>
+            <Text>Home Page</Text>
+        </View>
+    )
+}
